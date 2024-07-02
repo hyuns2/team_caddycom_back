58a5d3d5df4d4fa30d6555d48225981cfb953a4f
kwonssshyeon <kwonsh4396@gmail.com>
2024-07-02 14:52:53 +0900

    initial commit

---
+++ src/test/java/com/flash21/caddycom/CaddycomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flash21.caddycom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaddycomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2b2711577cf252127e670027fe39924e0efddb1f
kwonssshyeon <kwonsh4396@gmail.com>
2024-07-04 16:20:54 +0900

    Docs: 골프장 관련 swagger 수정

--- src/main/java/com/flash21/caddycom/controller/golfField/GolfFieldController.java
+++ src/main/java/com/flash21/caddycom/controller/golfField/GolfFieldController.java
@@ -11,7 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "Sample", description = "샘플 API")
+@Tag(name = "GolfField", description = "골프장 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/golf-field")
@@ -21,9 +21,9 @@ public class GolfFieldController {
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
                 produces = MediaType.APPLICATION_JSON_VALUE)
-    @Operation(summary="골프장 등록 API")
-    public ResponseEntity<?> registerGolfField(@ModelAttribute @Valid GolfFieldRequest request){
+    @Operation(summary="골프장 등록")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void registerGolfField(@ModelAttribute @Valid GolfFieldRequest request){
         golfFieldService.registerGolfField(request);
-        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }

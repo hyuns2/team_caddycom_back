13789464faa899776d88c216857defd2e0ee440e
hyun <hyunsoi0103@gmail.com>
2024-07-05 15:37:10 +0900

    Feat: 코스 조회기능 생성

---
+++ src/main/java/com/flash21/caddycom/controller/CourseController.java
@@ -0,0 +1,33 @@
+package com.flash21.caddycom.controller;
+
+import com.flash21.caddycom.dto.CourseInfoResponseDto;
+import com.flash21.caddycom.dto.ReservationSheetDto;
+import com.flash21.caddycom.service.CourseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "2. Course", description = "코스 API")
+@RequestMapping("/api/course")
+public class CourseController {
+    final CourseService courseService;
+
+    @Operation(summary = "코스 전체조회", description = "모든 코스정보를 조회합니다.")
+    @GetMapping
+    public ResponseEntity<?> retrieveCourseInfo() {
+        List<CourseInfoResponseDto> responseDtoList = courseService.retrieveCourseInfo();
+
+        return new ResponseEntity<>(responseDtoList, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/flash21/caddycom/service/CourseService.java
@@ -0,0 +1,31 @@
+package com.flash21.caddycom.service;
+
+import com.flash21.caddycom.dto.CourseInfoResponseDto;
+import com.flash21.caddycom.entity.Course;
+import com.flash21.caddycom.repository.CourseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CourseService {
+    final CourseRepository courseRepository;
+
+    public List<CourseInfoResponseDto> retrieveCourseInfo() {
+        List<Course> courseList = courseRepository.findAll();
+
+        List<CourseInfoResponseDto> returnDtoList = new ArrayList<>();
+        for (Course course: courseList) {
+            returnDtoList.add(CourseInfoResponseDto.builder().
+                    id(course.getId())
+                    .name(course.getName())
+                    .totalHoles(course.getTotalHoles()).build());
+        }
+        return returnDtoList;
+    }
+}

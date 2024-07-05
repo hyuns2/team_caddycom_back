02077c9186ea74ce306327713cdc6f38765660a0
hyun <hyunsoi0103@gmail.com>
2024-07-05 10:49:03 +0900

    Feat: 캘린더 메타정보 조회기능 생성

--- src/main/java/com/flash21/caddycom/service/ReservationSheetService.java
+++ src/main/java/com/flash21/caddycom/service/ReservationSheetService.java
@@ -3,13 +3,17 @@ package com.flash21.caddycom.service;
 import com.flash21.caddycom.dto.ReservationSheetDto;
 import com.flash21.caddycom.entity.ReservationDate;
 import com.flash21.caddycom.entity.ReservationSheet;
+import com.flash21.caddycom.repository.MetaDataReport;
 import com.flash21.caddycom.repository.ReservationDateRepository;
 import com.flash21.caddycom.repository.ReservationSheetRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +25,7 @@ public class ReservationSheetService {
         // Course 연결
         // 시간리스트 티오프리스트 사이즈 같은지 검증
         // 유효한 날짜와 시간인지 검증
+        // 시트 아이디 전부 반환
         List<ReservationSheet> sheets = ReservationSheetDto.CreateRequestDto.toEntities(dto);
         for (ReservationSheet sheet: sheets) {
             ReservationSheet returnSheet = rsRepository.save(sheet);
@@ -33,11 +38,30 @@ public class ReservationSheetService {
         for (LocalDate oneDay: dates) {
             rdRepository.save(ReservationDate.builder().
                     reservationSheet(sheet).
-                    reservationDate(oneDay).
+                    reservationAt(oneDay).
                     status(false).
                     totalCnt(0).
                     blockedCnt(0).build());
         }
+    }
+
+    public List<ReservationSheetDto.MetaDataResponseDto> retrieveMetaData(LocalDate targetDate, List<Long> reservationSheetIdList) {
+        List<ReservationSheetDto.MetaDataResponseDto> responseDtos = new ArrayList<>();
+        List<MetaDataReport> reports = rdRepository.countAllMetaDataByDate(targetDate, targetDate.plusMonths(1), reservationSheetIdList);
+
+        for (MetaDataReport report: reports) {
+            int totalCntResult = report.getTotalCntSum();
+            int blockedCntResult = report.getBlockedCntSum();
+            int availableCntResult = totalCntResult - blockedCntResult;
+
+            responseDtos.add(ReservationSheetDto.MetaDataResponseDto.builder().
+                    targetDate(report.getReservationAt()).
+                    totalCntSum(totalCntResult).
+                    blockedCntSum(blockedCntResult).
+                    availableCntSum(availableCntResult).build());
+        }
+
+        return responseDtos;
     }
 
 }

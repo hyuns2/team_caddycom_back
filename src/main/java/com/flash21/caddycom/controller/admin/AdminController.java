060c3e27bdab49e57bd0c54bbd9c61235b2b5294
kwonssshyeon <kwonsh4396@gmail.com>
2024-07-09 15:12:07 +0900

    Fix: Admin 기능 swagger 명세 수정

--- src/main/java/com/flash21/caddycom/controller/admin/AdminController.java
+++ src/main/java/com/flash21/caddycom/controller/admin/AdminController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@Tag(name = "Management", description = "전체 시스템 관리 API")
+@Tag(name = "0. Admin", description = "전체 시스템 관리 API")
 @RequestMapping("/api/admin")
 public class AdminController {
     private final AdminService adminService;

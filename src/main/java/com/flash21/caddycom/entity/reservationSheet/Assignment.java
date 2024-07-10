9b464c1bd81d43bd9d7ecdfbdb8016df2df13101
hyun <hyunsoi0103@gmail.com>
2024-07-10 10:15:43 +0900

    Feat: reservationSheet jdbcRepository 추가

--- src/main/java/com/flash21/caddycom/entity/reservationSheet/Assignment.java
+++ src/main/java/com/flash21/caddycom/entity/reservationSheet/Assignment.java
@@ -26,6 +26,7 @@ public class Assignment {
     private LocalTime startTime;
 
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private AssignmentStatus status;
 
 //    @OneToOne(fetch = FetchType.LAZY)

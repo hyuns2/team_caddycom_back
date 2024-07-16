ee4f48a2ec6d48fa97ff127fb29f279e5172114a
kwonssshyeon <kwonsh4396@gmail.com>
2024-07-16 17:29:05 +0900

    Fix: 응답 type string to json

--- src/main/java/com/flash21/caddycom/controller/golfField/GolfFieldController.java
+++ src/main/java/com/flash21/caddycom/controller/golfField/GolfFieldController.java
@@ -1,5 +1,6 @@
 package com.flash21.caddycom.controller.golfField;
 
+import com.flash21.caddycom.dto.Message;
 import com.flash21.caddycom.dto.golfField.FacilityRequest;
 import com.flash21.caddycom.dto.golfField.FacilityResponse;
 import com.flash21.caddycom.dto.golfField.GolfFieldRequest;
@@ -28,48 +29,48 @@ public class GolfFieldController {
                 produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary="골프장 등록 API", description = "골프장 관리자 or 전체 시스템 관리자는 골프장을 등록한다.")
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<String> create(@Valid @ModelAttribute GolfFieldRequest.Create request){
+    public ResponseEntity<Message> create(@Valid @ModelAttribute GolfFieldRequest.Create request){
         golfFieldService.createGolfField(request);
-        return ResponseEntity.ok().body("골프장이 등록되었습니다.");
+        return ResponseEntity.ok().body(new Message("골프장이 등록되었습니다."));
     }
 
 
     @ResponseStatus(HttpStatus.OK)
     @DeleteMapping
     @Operation(summary = "골프장 삭제 API", description="전체 시스템 관리자는 골프장을 삭제한다.")
-    public ResponseEntity<String> delete(@RequestParam Long golfFieldId) {
+    public ResponseEntity<Message> delete(@RequestParam Long golfFieldId) {
         golfFieldService.deleteGolfField(golfFieldId);
-        return ResponseEntity.ok().body("골프장이 삭제되었습니다..");
+        return ResponseEntity.ok().body(new Message("골프장이 삭제되었습니다.."));
     }
 
 
     @ResponseStatus(HttpStatus.OK)
     @PutMapping
     @Operation(summary = "골프장 수정 API", description="골프장 관리자 or 전체 시스템 관리자는 골프장 정보를 수정한다.")
-    public ResponseEntity<String> update(@RequestParam Long golfFieldId,
+    public ResponseEntity<Message> update(@RequestParam Long golfFieldId,
                                        @Valid @ModelAttribute GolfFieldRequest.Update request) {
         golfFieldService.updateGolfField(golfFieldId, request);
-        return ResponseEntity.ok().body("골프장이 수정되었습니다.");
+        return ResponseEntity.ok().body(new Message("골프장이 수정되었습니다."));
     }
 
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("detail-info")
     @Operation(summary = "골프장 추가정보 입력 API", description="골프장 관리자 or 전체 시스템 관리자는 골프장 추가정보를 입력한다.")
-    public ResponseEntity<String> addInfo(@RequestParam Long golfFieldId,
+    public ResponseEntity<Message> addInfo(@RequestParam Long golfFieldId,
                                         @Valid @RequestBody GolfFieldRequest.AdditionalInfo request) {
         golfFieldService.addMoreInfo(golfFieldId, request);
-        return ResponseEntity.ok().body("골프장 추가정보가 등록되었습니다.");    
+        return ResponseEntity.ok().body(new Message("골프장 추가정보가 등록되었습니다."));
     }
 
 
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("direction-info")
     @Operation(summary = "골프장 오는 길 입력 API", description="골프장 관리자 or 전체 시스템 관리자는 골프장 오는 길 안내를 입력한다.")
-    public ResponseEntity<String> addDirectionInfo(@RequestParam Long golfFieldId,
+    public ResponseEntity<Message> addDirectionInfo(@RequestParam Long golfFieldId,
                                                  @Valid @RequestBody GolfFieldRequest.DirectionsInfo request) {
         golfFieldService.addDirectionInfo(golfFieldId, request);
-        return ResponseEntity.ok().body("골프장 오는길이 등록되었습니다.");
+        return ResponseEntity.ok().body(new Message("골프장 오는길이 등록되었습니다."));
     }
 
 
@@ -77,10 +78,10 @@ public class GolfFieldController {
     @PostMapping(value = "facility-info",consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "골프장 시설정보 입력 API", description="골프장 관리자 or 전체 시스템 관리자는 골프장 시설 안내를 입력한다.")
-    public ResponseEntity<String> addFacilityInfo(@RequestParam Long golfFieldId,
+    public ResponseEntity<Message> addFacilityInfo(@RequestParam Long golfFieldId,
                                                 @Valid @ModelAttribute FacilityRequest.Create request) {
         golfFieldService.addFacilityInfo(golfFieldId, request);
-        return ResponseEntity.ok().body("골프장 시설 정보가 등록되었습니다.");  
+        return ResponseEntity.ok().body(new Message("골프장 시설 정보가 등록되었습니다."));
     }
 
 
@@ -111,9 +112,9 @@ public class GolfFieldController {
     @PutMapping(value = "facility-info",consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "골프장 시설정보 수정 API", description="골프장 관리자 or 전체 시스템 관리자는 골프장 시설 안내를 입력한다.")
-    public ResponseEntity<String> updateFacilityInfo(@RequestParam Long golfFieldId,
+    public ResponseEntity<Message> updateFacilityInfo(@RequestParam Long golfFieldId,
                                                    @Valid @ModelAttribute FacilityRequest.Update request) {
         golfFieldService.updateFacilityInfo(golfFieldId, request);
-        return ResponseEntity.ok().body("골프장 시설정보가 수정되었습니다.");
+        return ResponseEntity.ok().body(new Message("골프장 시설정보가 수정되었습니다."));
     }
 }

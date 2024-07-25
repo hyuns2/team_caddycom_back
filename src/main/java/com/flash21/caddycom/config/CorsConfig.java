0beeff14483bba41620cf4e4dde82f621aa736e6
kwonssshyeon <kwonsh4396@gmail.com>
2024-07-25 12:48:40 +0900

    Feat: CORS 허용 설정

---
+++ src/main/java/com/flash21/caddycom/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.flash21.caddycom.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .maxAge(3600);
+    }
+}

710105d535ad02ffdd68781ff370d7fce50ec611
LSBsity <dltmdqls51@naver.com>
2024-07-30 13:48:22 +0900

    feat: 캐디<->배정정보 연관관계 매핑

--- src/main/java/com/flash21/caddycom/entity/reservationSheet/Assignment.java
+++ src/main/java/com/flash21/caddycom/entity/reservationSheet/Assignment.java
@@ -1,5 +1,6 @@
 package com.flash21.caddycom.entity.reservationSheet;
 
+import com.flash21.caddycom.entity.caddy.HouseCaddy;
 import com.flash21.caddycom.entity.schedule.Schedule;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -33,9 +34,9 @@ public class Assignment {
     @Column(nullable = false)
     private AssignmentStatus status;
 
-//    @OneToOne(fetch = FetchType.LAZY)
-//    @JoinColumn
-//    Caddy caddy;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "house_caddy_id")
+    HouseCaddy caddy;
 
     @Column
     private String caddyName;
@@ -51,5 +52,11 @@ public class Assignment {
     public void cancelBlockAssignment(String reason) {
         this.status = AssignmentStatus.NOTHING;
         this.reason = "";
+    }
+
+    public void assignCaddy(HouseCaddy caddy) {
+        this.caddyName = caddy.getName();
+        this.caddy = caddy;
+        caddy.getAssignmentList().add(this);
     }
 }

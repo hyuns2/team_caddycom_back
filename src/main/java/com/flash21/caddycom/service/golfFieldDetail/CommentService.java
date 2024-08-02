c3561e052fcf2f525b36e61ce9ea9cfa3ad67625
Koo-EunSung <ku903@naver.com>
2024-08-02 13:56:05 +0900

    Comment: CommentService 주석 추가

--- src/main/java/com/flash21/caddycom/service/golfFieldDetail/CommentService.java
+++ src/main/java/com/flash21/caddycom/service/golfFieldDetail/CommentService.java
@@ -13,12 +13,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+/**
+ * 멘트 정보와 관련된 CRUD
+ *
+ * @author Koo-EunSung
+ */
 @Service
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
     private final HoleRepository holeRepository;
 
+    /**
+     * 멘트 정보를 생성하거나 수정한다.
+     *
+     * @param holeId 멘트가 포함되는 홀의 id
+     * @param tipInfos 설정한 멘트 정보 DTO
+     * @throws NoSuchElementException
+     *          멘트 정보를 설정할 홀이 존재하지 않는 경우
+     */
     @Transactional
     public void createAndUpdateComments(Long holeId, List<CommentDto.Info> tipInfos) {
         Hole hole = holeRepository.findById(holeId).orElseThrow(() -> new NoSuchElementException("해당 홀은 존재하지 않습니다."));
@@ -42,11 +55,22 @@ public class CommentService {
         savedComments.addAll(newComments);
     }
 
+    /**
+     * 멘트를 삭제한다.
+     *
+     * @param tipInfoIds 삭제할 멘트의 id 리스트
+     */
     @Transactional
     public void deleteComments(List<Long> tipInfoIds) {
         commentRepository.deleteAllByIdInBatch(tipInfoIds);
     }
 
+    /**
+     * 홀의 모든 멘트 정보를 조회한다.
+     *
+     * @param holeId 멘트 정보를 조회할 홀의 id
+     * @return 멘트 정보 DTO 리스트
+     */
     @Transactional(readOnly = true)
     public List<CommentDto.Info> getAllComments(Long holeId) {
         List<Comment> comments = commentRepository.findAllByHoleId(holeId)

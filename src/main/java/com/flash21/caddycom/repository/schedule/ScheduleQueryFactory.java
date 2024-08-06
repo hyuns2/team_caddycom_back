f2b07ad468c825ed62c5a7ace0cdc8f638932ef2
LSBsity <dltmdqls51@naver.com>
2024-08-06 09:41:14 +0900

    Fix: Deprecated 변경

--- src/main/java/com/flash21/caddycom/repository/schedule/ScheduleQueryFactory.java
+++ src/main/java/com/flash21/caddycom/repository/schedule/ScheduleQueryFactory.java
@@ -1,5 +1,7 @@
 package com.flash21.caddycom.repository.schedule;
 
+import com.flash21.caddycom.entity.caddy.Caddy;
+import com.flash21.caddycom.entity.caddy.QCaddy;
 import com.flash21.caddycom.entity.schedule.Schedule;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDate;
 import java.util.List;
 
+import static com.flash21.caddycom.entity.caddy.QCaddy.*;
 import static com.flash21.caddycom.entity.caddy.QHouseCaddy.houseCaddy;
 import static com.flash21.caddycom.entity.schedule.QAssignment.assignment;
 import static com.flash21.caddycom.entity.schedule.QSchedule.schedule;
@@ -21,7 +24,7 @@ public class ScheduleQueryFactory {
     public List<Schedule> findAllByDateFetchJoinToAssignmentAndHouseCaddy(Long golfFieldId, LocalDate date) {
         return jpaQueryFactory.selectFrom(schedule)
                 .leftJoin(schedule.assignments, assignment).fetchJoin()
-                .leftJoin(assignment.houseCaddy, houseCaddy).fetchJoin()
+                .leftJoin(assignment.caddy, caddy).fetchJoin()
                 .where(schedule.golfField.id.eq(golfFieldId)
                         .and(schedule.reservationAt.eq(date)))
                 .orderBy(schedule.course.id.asc())

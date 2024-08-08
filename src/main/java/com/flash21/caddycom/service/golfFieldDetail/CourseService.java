7d5321a92929fbd9f2e8a377efbf06126b4007a2
Koo-EunSung <ku903@naver.com>
2024-08-08 15:58:55 +0900

    Feat: Course 삭제 시 ReservationSheet의 코스 리스트 수정

--- src/main/java/com/flash21/caddycom/service/golfFieldDetail/CourseService.java
+++ src/main/java/com/flash21/caddycom/service/golfFieldDetail/CourseService.java
@@ -6,6 +6,7 @@ import com.flash21.caddycom.dto.golfFieldDetail.course.CourseResponse;
 import com.flash21.caddycom.entity.golfFieldDetail.Course;
 import com.flash21.caddycom.entity.golfFieldDetail.Formation;
 import com.flash21.caddycom.entity.schedule.AssignmentStatus;
+import com.flash21.caddycom.entity.schedule.ReservationSheet;
 import com.flash21.caddycom.repository.golfFieldDetail.CommentRepository;
 import com.flash21.caddycom.repository.golfFieldDetail.course.CourseRepository;
 import com.flash21.caddycom.repository.golfFieldDetail.hole.HoleRepository;
@@ -20,6 +21,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Stream;
 
 /**
  * 코스 정보와 관련된 CRUD
@@ -144,6 +146,24 @@ public class CourseService {
         entityManager.createQuery("DELETE from Schedule s where s.id in :scheduleIds")
                 .setParameter("scheduleIds", deleteScheduleId)
                 .executeUpdate();
+        //4. ReservationSheet에서 코스 삭제
+        //4-1. ReservationSheet 가져오기 (startDate > today + courseIdList에 삭제된 코스를 가지고 있는)
+        List<ReservationSheet> reservationSheets = entityManager.createQuery("SELECT r from ReservationSheet r where r.startDate > :today", ReservationSheet.class)
+                .setParameter("today", today)
+                .getResultList();
+        //4-2. reservationSheet의 courseIdList에서 삭제된 코스 id 삭제
+        for(ReservationSheet rs : reservationSheets) {
+            List<Long> newCourseIdList = rs.getCourseIdList().stream().flatMap(courseId -> {
+                if (ids.contains(courseId))
+                    return Stream.empty();
+                else
+                    return Stream.of(courseId);
+            }).toList();
+            entityManager.createQuery("UPDATE ReservationSheet r SET r.courseIdList = :updateCourseId where r.id = :id")
+                    .setParameter("updateCourseId", newCourseIdList)
+                    .setParameter("id", rs.getId())
+                    .executeUpdate();
+        }
     }
 
 

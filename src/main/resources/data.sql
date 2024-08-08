f08e10ae7a78af457c35924783c70270e34d46d8
kwonssshyeon <kwonsh4396@gmail.com>
2024-08-08 17:34:15 +0900

    Fix: 더미 데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -218,4 +218,7 @@ insert into `caddycom`.`tee` (id, hole_id, name, distance) values
 
 
 insert into `caddycom`.`caddy` (id, name, phone_number, golf_field_id, ctype, password)
-values (1, '더미캐디', '01011111111', 1, 'H', '111111')
+value (1, '더미캐디', '01011111111', 1, 'H', '111111');
+
+insert into `caddycom`.`caddy` (id, name, phone_number, ctype, password)
+value (2, '프리캐디', '01055555555', 'F', '111111');

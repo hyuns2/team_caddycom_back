b029272b9c103c6d08f44983d73fca2c57e4455b
kwonssshyeon <kwonsh4396@gmail.com>
2024-08-08 17:32:33 +0900

    Fix: 캐디 공통 메소드 상위 레포지토리로 이동

---
+++ src/main/java/com/flash21/caddycom/repository/caddy/FreeCaddyRepository.java
@@ -0,0 +1,7 @@
+package com.flash21.caddycom.repository.caddy;
+
+import com.flash21.caddycom.entity.caddy.FreeCaddy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FreeCaddyRepository extends JpaRepository<FreeCaddy, Long> {
+}

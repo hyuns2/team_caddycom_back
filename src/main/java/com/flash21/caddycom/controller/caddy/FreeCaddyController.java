55bf9ab22b66f1e16d65ea10d6f9f75ce0ecd8c3
kwonssshyeon <kwonsh4396@gmail.com>
2024-08-12 14:11:53 +0900

    Feat: 프리캐디 등록 스켈레톤 코드 작성

--- src/main/java/com/flash21/caddycom/controller/caddy/FreeCaddyController.java
+++ src/main/java/com/flash21/caddycom/controller/caddy/FreeCaddyController.java
@@ -1,13 +1,26 @@
 package com.flash21.caddycom.controller.caddy;
 
+import com.flash21.caddycom.dto.Message;
+import com.flash21.caddycom.dto.caddy.FreeCaddyRequest;
+import com.flash21.caddycom.service.caddy.FreeCaddyService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "5. Free Caddy", description = "프리캐디 API")
 @RequestMapping("/api/free-caddy")
 public class FreeCaddyController {
+    private final FreeCaddyService freeCaddyService;
+    @Operation(summary = "프리 캐디 등록", description = "프리 캐디의 정보를 저장한다.")
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Message> saveFreeCaddy(@Valid @ModelAttribute FreeCaddyRequest.Create request) {
+        freeCaddyService.saveFreeCaddy(request);
+        return ResponseEntity.ok().body(new Message("프리 캐디 등록 성공"));
+    }
 }

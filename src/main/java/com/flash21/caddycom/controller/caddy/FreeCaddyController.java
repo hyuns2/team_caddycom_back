74a1df5f0102081ecdf6526ed542216dd6348e6c
kwonssshyeon <kwonsh4396@gmail.com>
2024-08-14 16:40:37 +0900

    Refac: 프리캐디 등록 phone number formatting

--- src/main/java/com/flash21/caddycom/controller/caddy/FreeCaddyController.java
+++ src/main/java/com/flash21/caddycom/controller/caddy/FreeCaddyController.java
@@ -1,6 +1,7 @@
 package com.flash21.caddycom.controller.caddy;
 
 import com.flash21.caddycom.dto.Message;
+import com.flash21.caddycom.dto.caddy.FreeCaddyCommand;
 import com.flash21.caddycom.dto.caddy.FreeCaddyRequest;
 import com.flash21.caddycom.dto.caddy.FreeCaddyResponse;
 import com.flash21.caddycom.service.caddy.FreeCaddyService;
@@ -22,7 +23,7 @@ public class FreeCaddyController {
     @Operation(summary = "프리 캐디 등록", description = "프리 캐디의 정보를 저장한다.")
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Message> saveFreeCaddy(@Valid @ModelAttribute FreeCaddyRequest.Create request) {
-        freeCaddyService.saveFreeCaddy(request);
+        freeCaddyService.saveFreeCaddy(FreeCaddyCommand.Create.from(request));
         return ResponseEntity.ok().body(new Message("프리 캐디 등록 성공"));
     }
 

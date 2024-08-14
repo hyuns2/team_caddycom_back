56ad201e52a9d9db0f852724d73e766f503d8255
Koo-EunSung <ku903@naver.com>
2024-08-14 10:47:39 +0900

    HOTFIX: TeeDto.Info 어노테이션 누락 문제

--- src/main/java/com/flash21/caddycom/dto/golfFieldDetail/tee/TeeDto.java
+++ src/main/java/com/flash21/caddycom/dto/golfFieldDetail/tee/TeeDto.java
@@ -2,10 +2,14 @@ package com.flash21.caddycom.dto.golfFieldDetail.tee;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 public class TeeDto {
     @Getter
     @AllArgsConstructor
+    @Setter
+    @NoArgsConstructor
     public static class Info {
         private Long id;
         private String name;

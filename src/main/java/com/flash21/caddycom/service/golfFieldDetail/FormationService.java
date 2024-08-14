4cfee3abaf7b36e997c0c678375e359d0a3fc20d
LSBsity <dltmdqls51@naver.com>
2024-08-14 13:26:37 +0900

    Refactor: 코드 정렬

--- src/main/java/com/flash21/caddycom/service/golfFieldDetail/FormationService.java
+++ src/main/java/com/flash21/caddycom/service/golfFieldDetail/FormationService.java
@@ -9,7 +9,6 @@ import com.flash21.caddycom.entity.golfFieldDetail.Course;
 import com.flash21.caddycom.entity.golfFieldDetail.Formation;
 import com.flash21.caddycom.entity.golfFieldDetail.Hole;
 import com.flash21.caddycom.repository.golfField.GolfFieldRepository;
-import com.flash21.caddycom.repository.golfFieldDetail.CommentRepository;
 import com.flash21.caddycom.repository.golfFieldDetail.comment.CommentRepository;
 import com.flash21.caddycom.repository.golfFieldDetail.course.CourseRepository;
 import com.flash21.caddycom.repository.golfFieldDetail.formation.FormationRepository;
@@ -51,31 +50,28 @@ public class FormationService {
     EntityManager entityManager;
 
     public void processCreate(FormationRequest.Process request) {
-        GolfField golfField = golfFieldRepository.findById(request.getGolfFieldId())
-                .orElseThrow(() -> new NoSuchElementException("해당 골프장이 존재하지 않습니다."));
+        GolfField golfField = golfFieldRepository.findById(request.getGolfFieldId()).orElseThrow(() -> new NoSuchElementException("해당 골프장이 존재하지 않습니다."));
 
-        if(request.getCreate() != null)
-            for(FormationRequest.Create create : request.getCreate())
-                createFormation(golfField, create);
+        if (request.getCreate() != null) for (FormationRequest.Create create : request.getCreate())
+            createFormation(golfField, create);
 
-        if(request.getUpdate() != null)
-            for(FormationRequest.Update update : request.getUpdate())
-                updateFormation(update);
+        if (request.getUpdate() != null) for (FormationRequest.Update update : request.getUpdate())
+            updateFormation(update);
 
     }
+
     /**
      * 구성 정보를 생성하면서 코스, 홀, 티의 정보를 같이 생성한다.
      *
      * @param golfField 구성을 추가할 골프장의 id. null일 수 없다.
-     * @param request 구성 생성 요청 DTO
+     * @param request   구성 생성 요청 DTO
      */
     @Transactional
     public void createFormation(GolfField golfField, FormationRequest.Create request) {
         Formation formation;
-        if(request.getName() == null || request.getName().isBlank())
+        if (request.getName() == null || request.getName().isBlank())
             formation = new Formation(null, golfField, "NONE", null);
-        else
-            formation = new Formation(null, golfField, request.getName(), null);
+        else formation = new Formation(null, golfField, request.getName(), null);
 
         formationRepository.save(formation);
 
@@ -85,7 +81,7 @@ public class FormationService {
         holeService.createHoles(courses);
 
         List<Long> courseIds = new ArrayList<>();
-        for(Course course : courses)
+        for (Course course : courses)
             courseIds.add(course.getId());
 
         List<Hole> holes = holeRepository.findAllByCourseIds(courseIds);
@@ -97,36 +93,28 @@ public class FormationService {
      * 구성 정보를 수정한다. 구성에 포함된 코스 정보도 포함된다.
      *
      * @param request 구성 정보 수정 요청 DTO
-     * @throws NoSuchElementException
-     *          수정하려는 구성이 존재하지 않을 경우
-     * @throws IllegalArgumentException
-     *          구성의 이름을 공백으로 수정하려는 경우
+     * @throws NoSuchElementException   수정하려는 구성이 존재하지 않을 경우
+     * @throws IllegalArgumentException 구성의 이름을 공백으로 수정하려는 경우
      */
     @Transactional
     public void updateFormation(FormationRequest.Update request) {
-        Formation formation = formationRepository.findById(request.getFormationId())
-                .orElseThrow(() -> new NoSuchElementException("해당 구성은 존재하지 않습니다."));
+        Formation formation = formationRepository.findById(request.getFormationId()).orElseThrow(() -> new NoSuchElementException("해당 구성은 존재하지 않습니다."));
 
         String name = request.getFormationName();
         if (name != null) {
-            if (name.isBlank())
-                throw new IllegalArgumentException("구성의 이름은 공백일 수 없습니다.");
+            if (name.isBlank()) throw new IllegalArgumentException("구성의 이름은 공백일 수 없습니다.");
             formation.updateName(name);
         }
 
         List<CourseRequest.Update> courseUpdateInfos = request.getCourseInfos();
         List<CourseRequest.Create> courseCreateInfos = new ArrayList<>();
-        if(courseUpdateInfos != null) {
-            for(CourseRequest.Update courseUpdateInfo : courseUpdateInfos) {
+        if (courseUpdateInfos != null) {
+            for (CourseRequest.Update courseUpdateInfo : courseUpdateInfos) {
                 if (courseUpdateInfo.getId() == 0)
-                    courseCreateInfos.add(
-                            new CourseRequest.Create(courseUpdateInfo.getName(), courseUpdateInfo.getTotalHoles())
-                    );
-                else
-                    courseService.updateCourse(courseUpdateInfo);
+                    courseCreateInfos.add(new CourseRequest.Create(courseUpdateInfo.getName(), courseUpdateInfo.getTotalHoles()));
+                else courseService.updateCourse(courseUpdateInfo);
             }
-            if (!courseCreateInfos.isEmpty())
-                courseService.createCourses(formation, courseCreateInfos);
+            if (!courseCreateInfos.isEmpty()) courseService.createCourses(formation, courseCreateInfos);
         }
     }
 
@@ -137,9 +125,7 @@ public class FormationService {
      */
     @Transactional
     public void deleteFormations(List<Long> ids) {
-        List<Long> courseIds = entityManager.createQuery("SELECT c.id from Course c where c.formation.id in :formationIds", Long.class)
-                        .setParameter("formationIds", ids)
-                        .getResultList();
+        List<Long> courseIds = entityManager.createQuery("SELECT c.id from Course c where c.formation.id in :formationIds", Long.class).setParameter("formationIds", ids).getResultList();
         courseService.deleteCourses(courseIds);
         formationRepository.deleteAllByIdInBatch(ids);
     }
@@ -149,18 +135,16 @@ public class FormationService {
      *
      * @param golfFieldId 구성 정보를 조회할 골프장의 id. null일 수 없다.
      * @return 골프장의 모든 구성 정보
-     * @throws NoSuchElementException
-     *          골프장에 구성이 존재하지 않는 경우
+     * @throws NoSuchElementException 골프장에 구성이 존재하지 않는 경우
      */
     public List<FormationResponse.Info> getAllFormations(Long golfFieldId) {
         List<Formation> formations = formationRepository.findAllByGolfFieldId(golfFieldId);
-        if(formations.isEmpty())
-            throw new NoSuchElementException("골프장에 구성이 존재하지 않습니다.");
+        if (formations.isEmpty()) throw new NoSuchElementException("골프장에 구성이 존재하지 않습니다.");
 
         List<FormationResponse.Info> response = new ArrayList<>();
-        for(Formation formation : formations) {
+        for (Formation formation : formations) {
             List<CourseResponse.Info> courseInfos = new ArrayList<>();
-            if(!formation.getCourses().isEmpty()) {
+            if (!formation.getCourses().isEmpty()) {
                 courseInfos = formation.getCourses().stream()
                         .map(course -> new CourseResponse.Info(course.getId(), course.getName(), course.getTotalHoles()))
                         .sorted(Comparator.comparingLong(CourseResponse.Info::getId))

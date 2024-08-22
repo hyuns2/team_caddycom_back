b632620daf924ff2b36308716ae8f60cbd25d88f
kwonssshyeon <kwonsh4396@gmail.com>
2024-08-22 13:56:25 +0900

    Docs: enum 주석 추가

--- src/main/java/com/flash21/caddycom/entity/schedule/AssignmentStatus.java
+++ src/main/java/com/flash21/caddycom/entity/schedule/AssignmentStatus.java
@@ -6,13 +6,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum AssignmentStatus {
-    NOTHING(0),
-    BLOCKED(1),
-    CANCELED(2),
-    ASSIGNED(3),
-    CANCEL_REQUESTED(4),
-    DELETED(5),
-    ASSIGN_REQUESTED(6);
+    NOTHING(0),             //아무것도 없는 초기 상태
+    BLOCKED(1),             //블락된 상태
+    CANCELED(2),            //취소된 상태
+    ASSIGNED(3),            //배정된 상태
+    CANCEL_REQUESTED(4),    //취소 요청된 상태
+    DELETED(5),             //삭제된 상태
+    ASSIGN_REQUESTED(6);    //프리캐디에 배정 요청된 상태
 
     private final int number;
 }
--- src/main/java/com/flash21/caddycom/entity/schedule/DateStatus.java
+++ src/main/java/com/flash21/caddycom/entity/schedule/DateStatus.java
@@ -3,6 +3,9 @@ package com.flash21.caddycom.entity.schedule;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * 예약시트 생성시 수백개의 assignment가 생기는데 지연 생성을 위해 assignment가 생겼는지 아닌지를 나타내는 enum
+ */
 @Getter
 @AllArgsConstructor
 public enum DateStatus {
